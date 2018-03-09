e56ba0b76b06a6c2e0dc0126b325d5ad8ebe824d
woldt <woldtensae.aman@gmail.com>
2018-03-08 16:01:34 -0800

    checking branch commit

--- src/main/java/ass/apex/controller/AppointmentController.java
+++ src/main/java/ass/apex/controller/AppointmentController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import ass.apex.service.AppointmentService;
 
@@ -13,7 +14,7 @@ public class AppointmentController {
 	@Autowired
 	AppointmentService appointmentService;
 	
-	@RequestMapping("/")
+	@RequestMapping(value = "/", method = RequestMethod.GET)
     public String listAssignment(Model model) {
 	 		model.addAttribute("allAssignment", appointmentService.getAppointments());
 	 	return "appointment";	
--- src/main/java/ass/apex/domain/Appointment.java
+++ src/main/java/ass/apex/domain/Appointment.java
@@ -7,7 +7,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
-@Entity
+//@Entity
 public class Appointment {
 	
 	@Id
