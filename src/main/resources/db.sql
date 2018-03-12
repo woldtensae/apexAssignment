ba60baf6466d3bfa0e7380af188d53fbf82be722
woldt <woldtensae.aman@gmail.com>
2018-03-12 08:11:55 -0700

    sql file adjustment

--- src/main/resources/db.sql
+++ src/main/resources/db.sql
@@ -1,7 +1,7 @@
 
-INSERT INTO appointment (id, date, description, time) VALUES (4,'1981-04-06', 'pray', '23:59');
-INSERT INTO appointment (id, date, description, time) VALUES (5,'1981-04-06', 'eat', '23:59');
-INSERT INTO appointment (id, date, description, time) VALUES (6,'1981-04-06', 'leave', '23:59');
+INSERT INTO appointment (id, date, description, time) VALUES (4,'2018-04-06', 'pray', '09:00');
+INSERT INTO appointment (id, date, description, time) VALUES (5,'2018-05-12', 'eat', '04:59');
+INSERT INTO appointment (id, date, description, time) VALUES (6,'2018-09-02', 'leave', '05:00');
 
 
 
